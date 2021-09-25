cd2999462c6780f693ae45ed9cb411763c59ac36
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-25 11:08:38 +0700

    Create RDP.sh

---
+++ RDP.sh
@@ -0,0 +1,18 @@
+echo "===================================="
+echo "Update"
+echo "===================================="
+apt update && apt upgrade -y > /dev/null 2>&1
+echo "===================================="
+echo "Install Gui"
+echo "===================================="
+apt install ubuntu-desktop -y > /dev/null 2>&1
+echo "===================================="
+echo "===================================="
+echo "Install RDP"
+echo "===================================="
+sudo apt install -y xrdp > /dev/null 2>&1
+sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+sudo service xrdp start > /dev/null 2>&1
+echo "===================================="
+echo "Finish"
+echo "===================================="
