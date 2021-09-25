c1e64dbb5a7ac01f12c96397c2ccc1b75edc2a35
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-25 11:35:24 +0700

    Update ubunturdp.sh

--- ubunturdp.sh
+++ ubunturdp.sh
@@ -5,12 +5,12 @@ echo -ne '>>>>>>>                   [40%]\r'
 apt upgrade -y > /dev/null 2>&1
 sleep 2
 echo -ne '>>>>>>>>>>>>>>            [60%]\r'
-apt install ubuntu-desktop -y
+apt install ubuntu-desktop -y > /dev/null 2>&1
 sleep 2
 echo -ne '>>>>>>>>>>>>>>>>>>>>>>>   [80%]\r'
-sudo apt install -y xrdp
-sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh
-sudo service xrdp start
+sudo apt install -y xrdp > /dev/null 2>&1
+sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+sudo service xrdp start > /dev/null 2>&1
 sleep 2
 echo -ne '>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\r'
 echo -ne '\n'
