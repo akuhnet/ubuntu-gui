2e555c69ade1da244d35abc12e4128b98bcb0015
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-27 14:23:13 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -9,6 +9,5 @@ apt wget -y > /dev/null 2>&1
 sleep 2
 echo -ne '>>>>>>>>>>>>>>>>>>>>>>>   [80%]\r'
 apt update -y > /dev/null 2>&1
-apt upgrade -y > /dev/null 2>&1
 echo -ne '>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\r'
 echo -ne '\n'
