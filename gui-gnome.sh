e76e63b7ac0c68a350aab604485fbc3d378046ec
akuh <81416541+akuhnet@users.noreply.github.com>
2021-10-08 08:11:39 +0700

    Update gui-gnome.sh

--- gui-gnome.sh
+++ gui-gnome.sh
@@ -2,3 +2,7 @@ apt-get update && apt-get install wget -y && DEBIAN_FRONTEND="noninteractive" TZ
 apt-get install ubuntu-desktop -y
 wget -O nomachine.deb https://bit.ly/nomachinev7
 sudo apt install ./nomachine.deb
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok.zip
+sudo adduser akuhnet --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo "akuhnet:123" | sudo chpasswd
