0d9eab03ec079de060d6e77467e8f3452f80fbaf
akuh <81416541+akuhnet@users.noreply.github.com>
2021-10-08 08:25:55 +0700

    Update gui-gnome.sh

--- gui-gnome.sh
+++ gui-gnome.sh
@@ -1,6 +1,6 @@
 apt-get update && apt-get install wget -y && DEBIAN_FRONTEND="noninteractive" TZ="America/New_York" apt-get install -y tzdata
-apt-get upgrade -y
-apt-get install ubuntu-desktop -y
+echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
+apt-get install -y -q && apt-get install ubuntu-desktop -y
 wget -O nomachine.deb https://bit.ly/nomachinev7
 sudo apt install ./nomachine.deb
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
